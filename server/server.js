1e0f01c6497d560250227d0bc99334cd5fb40360
Lavine Irvine <lavineirvine@Lavines-MacBook-Pro.local>
2021-05-27 11:36:23 +0200

    created schema using mongodb. This is saved under server>models.

--- server/server.js
+++ server/server.js
@@ -2,25 +2,40 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+
+//Models
+const User = require('./models/user');
+const Product = require('./models/product');
+
+
+dotenv.config();
 
 const app = express();
 
+mongoose.connect(
+    process.env.DATABASE, 
+    { useNewUrlParser: true, useUnifiedTopology: true  },
+    err => {
+        if(err){
+            console.log('Server connectio error');
+        } else {
+            console.log('Server connected');
+    }
+});
 //Middlewares
 app.use(morgan('dev'));
 //needed to parser data from frontend 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.json('hello beuva');
-});
+//REQUIRE API's
+const productRoutes = require('./routes/product');
+app.use("/api", productRoutes);
 
-app.post('/', (req, res) => {
-    var hair = ['brazilian', 'mongolian', 'indan remy'];
-    res.send(hair);
-    console.log(req.body.name);
-});
 
+//PORT location 
 app.listen(3000, (err) => {
     if(err) {
         console.log(err);
